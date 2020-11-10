b8234d11772719f90ff70dfdebcee38d003c631a
j.takubo <j.takubo@datumstudio.jp>
2020-11-10 20:53:58 +0900

    Updates

---
+++ component---src-pages-contact-js-2887ad418aea2d638dc6.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"Cuy+":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),c=n("Wbzz");t.default=function(){return l.a.createElement("div",{style:{color:"teal"}},l.a.createElement(c.Link,{to:"/"},"Home"),l.a.createElement("p",null,"Send us a message!"),l.a.createElement("h2",null,"ただいま、Gatsby勉強中：ブログのアプデもう少々お待ちください。m__m"))}}}]);
+//# sourceMappingURL=component---src-pages-contact-js-2887ad418aea2d638dc6.js.map

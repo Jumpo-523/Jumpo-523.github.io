f3126d98bdd44cea31e41bf290f0bbae96efcab0
nyk523jt@gmail.com <j.takubo@datumstudio.jp>
2020-11-11 21:30:08 +0900

    add my profile

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,10 +5,34 @@ export default () => (
   <div style={{ color: `teal` }}>
     <Link to="/">Home</Link>
     {/* <Header headerText="Contact" /> */}
-    <p>Send us a message!</p>
+    
 
-    <h2>
-      ただいま、Gatsby勉強中：ブログのアプデもう少々お待ちください。m__m
-    </h2>
+    <h1>Who am I ???</h1>
+    <h2>自己紹介</h2>
+    <p>name:田久保　純平<br></br>age: 28歳</p>
+    <h2>データベース</h2>
+    <p>Job: データ分析<br></br>
+        skill: Python, , etc...<br></br>
+        hobby: 漫画をよく読みます。chess<br></br>
+        </p>
+    <h2>やりたい事</h2>
+    <p>
+        IoTプログラミング / Deep learning実装
+    </p>
+    <h2> 勉強したい事</h2>
+        <p>
+            GO言語：<a href="https://go-tour-jp.appspot.com/welcome/1">Goのチュートリアルが出来るsite</a>
+        </p>
+    <h2>link</h2>
+    <p>
+      <a href="https://twitter.com/j_takurou">twitter</a>
+    </p>
+    <p>
+      <a href="https://github.com/Jumpo-523">github</a>
+    </p>
+    <p>
+      <a href="https://qiita.com/j_takurou">qitta</a>
+    </p>
+ 
   </div>
-)
+  )
